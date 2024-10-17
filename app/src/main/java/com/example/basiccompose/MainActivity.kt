b85579d17a4b75b01235a31e8d3418dedc7f5534
Alan21103 <syahlansaputra03@gmail.com>
2024-10-17 21:29:40 +0700

    Menambahkan style pada text login dan memanggil gambar logo umy dari resource

--- app/src/main/java/com/example/basiccompose/MainActivity.kt
+++ app/src/main/java/com/example/basiccompose/MainActivity.kt
@@ -4,18 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.basiccompose.ui.theme.BasicComposeTheme
 
@@ -43,6 +48,10 @@ fun BasicLayout(modifier: Modifier = Modifier){
         Text("Login", style = TextStyle(fontSize = 50.sp,
             fontWeight = FontWeight.Bold
         ))
+        Text("Ini adalah halaman Login", style = TextStyle(fontSize = 20.sp,
+            fontStyle = FontStyle.Italic))
+        Image(painter = painterResource(id = R.drawable.umy), contentDescription = null,
+            modifier = Modifier.padding(top = 40.dp).size(250.dp))
     }
 }
 

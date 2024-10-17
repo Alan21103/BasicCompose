a5ead4ceb1f24b5c4b389e3446b8030116015111
Alan21103 <syahlansaputra03@gmail.com>
2024-10-17 21:15:34 +0700

    Membuat class main activity dan menambahkan scaffold modifier innerpadding

---
+++ app/src/main/java/com/example/basiccompose/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.basiccompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.basiccompose.ui.theme.BasicComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            BasicComposeTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    BasicLayout(modifier = Modifier.padding(innerPadding))
+                }
+            }
+        }
+    }
+}
+
+fun BasicLayout(modifier: Modifier = Modifier){
+
+}

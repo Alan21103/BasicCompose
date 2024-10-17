7b91aff7b152d23b4ad1c0586556d946c6df96d9
Alan21103 <syahlansaputra03@gmail.com>
2024-10-17 21:25:30 +0700

    Menambahkan column dan teks pada compose dan menambahkan preview pada compose

--- app/src/main/java/com/example/basiccompose/MainActivity.kt
+++ app/src/main/java/com/example/basiccompose/MainActivity.kt
@@ -13,7 +13,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.basiccompose.ui.theme.BasicComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -37,7 +40,9 @@ fun BasicLayout(modifier: Modifier = Modifier){
         verticalArrangement = Arrangement.Center,
         modifier = Modifier.fillMaxSize()
     ){
-
+        Text("Login", style = TextStyle(fontSize = 50.sp,
+            fontWeight = FontWeight.Bold
+        ))
     }
 }
 
